09b6a685b4e15f01f57b84322d408e6246fcbd92
Danny Baumann <dannybaumann@web.de>
2012-09-17 11:11:53 +0200

    Move common stuff to common subdir.
    Change-Id: Ib96c5836a9e395e25ea88a9b6359abaab711094a

--- jordan/jordan-vendor.mk
+++ jordan/jordan-vendor.mk
@@ -102,46 +102,6 @@ PRODUCT_COPY_FILES += \
 	vendor/motorola/jordan/etc/motorola/comm_drv/plmn_text_table.bin:system/etc/motorola/comm_drv/plmn_text_table.bin \
 	vendor/motorola/jordan/etc/motorola/comm_drv/PLMN_VERSION.txt:system/etc/motorola/comm_drv/PLMN_VERSION.txt
 
-# basebands
-PRODUCT_COPY_FILES += \
-	vendor/motorola/jordan/etc/motorola/basebands/america/argentina_claro_342.zip:system/etc/motorola/basebands/america/argentina_claro_342.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/america/argentina_personal_342.zip:system/etc/motorola/basebands/america/argentina_personal_342.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/america/argentina_614.zip:system/etc/motorola/basebands/america/argentina_614.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/america/brasil_342.zip:system/etc/motorola/basebands/america/brasil_342.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/america/brasil_claro_342.zip:system/etc/motorola/basebands/america/brasil_claro_342.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/america/brasil_claro_614.zip:system/etc/motorola/basebands/america/brasil_claro_614.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/america/brasil_tim_342.zip:system/etc/motorola/basebands/america/brasil_tim_342.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/america/brasil_vivo_342.zip:system/etc/motorola/basebands/america/brasil_vivo_342.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/america/colombia_tigo_342.zip:system/etc/motorola/basebands/america/colombia_tigo_342.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/america/us_tmobile_342_4.zip:system/etc/motorola/basebands/america/us_tmobile_342_4.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/america/us_tmobile_342_9.zip:system/etc/motorola/basebands/america/us_tmobile_342_9.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/america/us_tmobile_6xx.zip:system/etc/motorola/basebands/america/us_tmobile_6xx.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/asia/china_25_26_210.zip:system/etc/motorola/basebands/asia/china_25_26_210.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/asia/china_026_110.zip:system/etc/motorola/basebands/asia/china_026_110.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/asia/china_me722_110.zip:system/etc/motorola/basebands/asia/china_me722_110.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/asia/hktaiwan_259.zip:system/etc/motorola/basebands/asia/hktaiwan_259.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/asia/sea_343_331.zip:system/etc/motorola/basebands/asia/sea_343_331.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/asia/sea_343_361.zip:system/etc/motorola/basebands/asia/sea_343_361.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/asia/southkorea_342.zip:system/etc/motorola/basebands/asia/southkorea_342.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/australia/optus_342.zip:system/etc/motorola/basebands/australia/optus_342.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/australia/telstra_226.zip:system/etc/motorola/basebands/australia/telstra_226.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/europe/central_europe_221.zip:system/etc/motorola/basebands/europe/central_europe_221.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/europe/central_europe_234.zip:system/etc/motorola/basebands/europe/central_europe_234.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/europe/central_europe_251.zip:system/etc/motorola/basebands/europe/central_europe_251.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/europe/central_europe_342.zip:system/etc/motorola/basebands/europe/central_europe_342.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/europe/central_europe_34x.zip:system/etc/motorola/basebands/europe/central_europe_34x.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/europe/france_orange_342.zip:system/etc/motorola/basebands/europe/france_orange_342.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/europe/france_vodafone_342.zip:system/etc/motorola/basebands/europe/france_vodafone_342.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/europe/italy_251.zip:system/etc/motorola/basebands/europe/italy_251.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/europe/poland_orange_251.zip:system/etc/motorola/basebands/europe/poland_orange_251.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/europe/poland_orange_342.zip:system/etc/motorola/basebands/europe/poland_orange_342.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/europe/uk_251.zip:system/etc/motorola/basebands/europe/uk_251.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/europe/uk_342.zip:system/etc/motorola/basebands/europe/uk_342.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/europe/uk_343_3.zip:system/etc/motorola/basebands/europe/uk_343_3.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/europe/uk_343_11.zip:system/etc/motorola/basebands/europe/uk_343_11.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/europe/uk_tmobile_221.zip:system/etc/motorola/basebands/europe/uk_tmobile_221.zip \
-	vendor/motorola/jordan/etc/motorola/basebands/europe/uk_tmobile_251.zip:system/etc/motorola/basebands/europe/uk_tmobile_251.zip
-
 # default baseband (central europe 3.4.x)
 PRODUCT_COPY_FILES += \
 	vendor/motorola/jordan/etc/motorola/bp_nvm_default/File_Audio:system/etc/motorola/bp_nvm_default/File_Audio \
@@ -205,48 +165,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 	vendor/motorola/jordan/media/Moto.ogg:system/media/audio/ringtones/Moto.ogg
 
-#vsuite
-PRODUCT_COPY_FILES += \
-	vendor/motorola/jordan/app/VSuiteApp.apk:system/app/VSuiteApp.apk \
-	vendor/motorola/jordan/etc/nuance/vsuite_config.xml:system/etc/nuance/vsuite_config.xml \
-	vendor/motorola/jordan/lib/libvsuite_mot_vs32_cmb103.so:system/lib/libvsuite_mot_vs32_cmb103.so \
-	vendor/motorola/jordan/tts/nuance/img_dede.bin:system/tts/nuance/img_dede.bin \
-	vendor/motorola/jordan/tts/nuance/img_engb.bin:system/tts/nuance/img_engb.bin \
-	vendor/motorola/jordan/tts/nuance/img_enus.bin:system/tts/nuance/img_enus.bin \
-	vendor/motorola/jordan/tts/nuance/img_eses.bin:system/tts/nuance/img_eses.bin \
-	vendor/motorola/jordan/tts/nuance/img_esmx.bin:system/tts/nuance/img_esmx.bin \
-	vendor/motorola/jordan/tts/nuance/img_frca.bin:system/tts/nuance/img_frca.bin \
-	vendor/motorola/jordan/tts/nuance/img_frfr.bin:system/tts/nuance/img_frfr.bin \
-	vendor/motorola/jordan/tts/nuance/img_itit.bin:system/tts/nuance/img_itit.bin \
-	vendor/motorola/jordan/tts/nuance/img_ptbr.bin:system/tts/nuance/img_ptbr.bin \
-	vendor/motorola/jordan/tts/nuance/lvr_dede.bin:system/tts/nuance/lvr_dede.bin \
-	vendor/motorola/jordan/tts/nuance/lvr_engb.bin:system/tts/nuance/lvr_engb.bin \
-	vendor/motorola/jordan/tts/nuance/lvr_enus.bin:system/tts/nuance/lvr_enus.bin \
-	vendor/motorola/jordan/tts/nuance/lvr_eses.bin:system/tts/nuance/lvr_eses.bin \
-	vendor/motorola/jordan/tts/nuance/lvr_esmx.bin:system/tts/nuance/lvr_esmx.bin \
-	vendor/motorola/jordan/tts/nuance/lvr_frca.bin:system/tts/nuance/lvr_frca.bin \
-	vendor/motorola/jordan/tts/nuance/lvr_frfr.bin:system/tts/nuance/lvr_frfr.bin \
-	vendor/motorola/jordan/tts/nuance/lvr_itit.bin:system/tts/nuance/lvr_itit.bin \
-	vendor/motorola/jordan/tts/nuance/lvr_ptbr.bin:system/tts/nuance/lvr_ptbr.bin \
-	vendor/motorola/jordan/tts/nuance/prm_dede.bin:system/tts/nuance/prm_dede.bin \
-	vendor/motorola/jordan/tts/nuance/prm_engb.bin:system/tts/nuance/prm_engb.bin \
-	vendor/motorola/jordan/tts/nuance/prm_enus.bin:system/tts/nuance/prm_enus.bin \
-	vendor/motorola/jordan/tts/nuance/prm_eses.bin:system/tts/nuance/prm_eses.bin \
-	vendor/motorola/jordan/tts/nuance/prm_esmx.bin:system/tts/nuance/prm_esmx.bin \
-	vendor/motorola/jordan/tts/nuance/prm_frca.bin:system/tts/nuance/prm_frca.bin \
-	vendor/motorola/jordan/tts/nuance/prm_frfr.bin:system/tts/nuance/prm_frfr.bin \
-	vendor/motorola/jordan/tts/nuance/prm_itit.bin:system/tts/nuance/prm_itit.bin \
-	vendor/motorola/jordan/tts/nuance/prm_ptbr.bin:system/tts/nuance/prm_ptbr.bin \
-	vendor/motorola/jordan/tts/nuance/str_dede.bin:system/tts/nuance/str_dede.bin \
-	vendor/motorola/jordan/tts/nuance/str_engb.bin:system/tts/nuance/str_engb.bin \
-	vendor/motorola/jordan/tts/nuance/str_enus.bin:system/tts/nuance/str_enus.bin \
-	vendor/motorola/jordan/tts/nuance/str_eses.bin:system/tts/nuance/str_eses.bin \
-	vendor/motorola/jordan/tts/nuance/str_esmx.bin:system/tts/nuance/str_esmx.bin \
-	vendor/motorola/jordan/tts/nuance/str_frca.bin:system/tts/nuance/str_frca.bin \
-	vendor/motorola/jordan/tts/nuance/str_frfr.bin:system/tts/nuance/str_frfr.bin \
-	vendor/motorola/jordan/tts/nuance/str_itit.bin:system/tts/nuance/str_itit.bin \
-	vendor/motorola/jordan/tts/nuance/str_ptbr.bin:system/tts/nuance/str_ptbr.bin
-
 #MTP
 PRODUCT_COPY_FILES += \
 	vendor/motorola/jordan/app/MediaSync.apk:system/app/MediaSync.apk \
@@ -260,3 +178,4 @@ PRODUCT_COPY_FILES += \
 	vendor/motorola/jordan/lib/libmot_atcmd.so:system/lib/libmot_atcmd.so \
 	vendor/motorola/jordan/lib/libmot_atcmd_mflex.so:system/lib/libmot_atcmd_mflex.so
 
+include vendor/motorola/jordan-common/jordan-common-vendor.mk
